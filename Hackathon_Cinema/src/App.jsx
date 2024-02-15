32af9e6d91bb43c0b963e0f2ad6624b549a9edb2
Lok Sze Chung <lschung@hotmail.co.uk>
2024-02-15 15:33:37 +0000

    Add navbar and home

--- Hackathon_Cinema/src/App.jsx
+++ Hackathon_Cinema/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route, } from "react-router-dom";
 import './App.css'
-import Home from "../src/pages/Home.jsx";
-import WhatsOn from "./pages/WhatsOn.jsx";
-import SignUp from "./pages/SignUp.jsx";
+import Home from "../src/pages/Home";
+import WhatsOn from "./pages/WhatsOn";
+import SignUp from "./pages/SignUp";
+import SiteNavbar from "./components/Navbar/SiteNavbar";
 
 function App() {
   
 
   return (
     <>
+      <SiteNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/whats-on" element={<WhatsOn />} />
---
+++ Hackathon_Cinema/src/pages/WhatsOn.jsx
@@ -0,0 +1,5 @@
+const WhatsOn = () => {
+
+}
+
+export default WhatsOn
