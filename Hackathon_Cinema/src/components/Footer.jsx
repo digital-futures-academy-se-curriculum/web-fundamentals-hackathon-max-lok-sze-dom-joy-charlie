1281f0974cbf28082471b64ed4970b0e39e0ab0b
Max <m.hutchings030@gmail.com>
2024-02-15 11:35:17 +0000

    Completed Footer

---
+++ Hackathon_Cinema/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import DFSignatureCopyright from "./footer/DFSignatureCopyright";
+import SocialMediaAdvert from "./footer/SocialMediaAdvert";
+
+
+export default function Footer() {
+    return (
+        <div className="container-fluid" style={{backgroundColor: "var(--secondary-color)"}}>
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-6">
+                    <SocialMediaAdvert />
+                </div>
+            </div>
+            <div className="row d-flex justify-content-center">
+                <div className="col">
+                    <DFSignatureCopyright />
+                </div>
+            </div>
+        </div>    
+    )
+}
---
+++ Hackathon_Cinema/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer.jsx";
+
+
+export default function Home(){
+    return(
+        <>
+        
+            <Footer />
+            
+        </>
+    )
+}
