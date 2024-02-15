a9c2bf94ce925d43b28e9bba7a5d192ecc9c07c0
Charlie Tait <charlietait147@gmail.com>
2024-02-15 15:52:18 +0000

    add footer icons and form and responsiveness

---
+++ Hackathon_Cinema/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () =>  {
+return (
+
+    <footer className="footer">
+     <div className="footer_container">
+        <div className="icons">
+         <img src="images/facebook-icon" alt="Facebook" className="facebook-icon" />
+         <img src="images/instagram-icon" alt="Instagram" className="instagram-icon" />
+         <img src="images/twitter-icon" alt="Twitter" className="twitter-icon" />
+         <img src="images/reddit-icon" alt="Reddit" className="reddit-icon" />
+         <img src="images/tiktok-icon" alt="Tiktok" className="tiktok-icon" />
+         </div>
+    <p class="footer-text"> &copy; DF Cinemas - 2021- </p>
+    </div>
+    </footer>
+
+);
+};
+
+export default Footer;
--- Hackathon_Cinema/src/pages/SignUp.jsx
+++ Hackathon_Cinema/src/pages/SignUp.jsx
@@ -1,8 +1,12 @@
+import SignUpForm from "../components/SignUp.jsx/SignUpForm"
 import ThreeImageSection from "../components/SignUp.jsx/ThreeImageSection"
 
 const SignUp = () => {
   return (
-    <ThreeImageSection />
+    <div className="d-sm-flex pt-sm-3">
+        <ThreeImageSection />
+        <SignUpForm />
+    </div>
   )
 }
 
